d1a82a3af667204243211e331eda13bb14a6a956
MangIndra1 <komangindrapradnya@gmail.com>
2025-06-18 01:56:49 +0800

    feat: Nambah konektor db mas

---
+++ db/db_connect.php
@@ -0,0 +1,14 @@
+<?php
+    // Database connection parameters
+    $db_host = 'localhost';
+    $db_user = 'root';
+    $db_pass = '';
+    $db_name = 'database_botany';
+
+    // Connection to the database
+    $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+    // Check connection
+    if (!$conn) {
+        die("Koneksi ke database gagal: " . mysqli_connect_error());
+    }
+?>
