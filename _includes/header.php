24866fa4010e328bffb642c7f061d469f593d712
MangIndra1 <komangindrapradnya@gmail.com>
2025-07-10 23:44:14 +0800

    feat: fix header.php

--- _includes/header.php
+++ _includes/header.php
@@ -1,4 +1,6 @@
 <?php
+// File: _includes/header.php (Versi Final)
+
 // Memeriksa apakah sesi sudah aktif sebelum memulainya.
 if (session_status() == PHP_SESSION_NONE) {
     session_start();
@@ -14,7 +16,7 @@ function getInitials($name) {
     if (isset($words[1]) && !empty($words[1])) {
         $initials .= strtoupper($words[1][0]);
     }
-    return $initials ?: 'U'; // Default 'U' jika nama kosong atau hanya spasi
+    return $initials ?: 'U'; // Default 'U' jika nama kosong
 }
 
 // Mendapatkan nama halaman saat ini untuk menu aktif
@@ -29,10 +31,9 @@ $current_page = basename($_SERVER['PHP_SELF']);
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="assets/css/style.css" />
-
 </head>
 <body class="<?php echo isset($body_class) ? $body_class : ''; ?>">
-    <nav class="navbar navbar-expand-lg shadow-sm py-3 sticky-top">
+    <nav class="navbar navbar-expand-lg bg-white shadow-sm py-3 sticky-top">
         <div class="container">
             <a class="navbar-brand" href="index.php">
                 <img src="assets/img/LogoTeks.png" alt="Botany Academy Logo" width="120" />
@@ -53,25 +54,21 @@ $current_page = basename($_SERVER['PHP_SELF']);
                     </li>
 
                     <?php if (isset($_SESSION['user_id'])) : ?>
-                        <li class="nav-item d-lg-none"><hr></li> <li class="nav-item d-lg-none">
-                            <a class="nav-link" href="dashboard.php">Pembelajaran Saya</a>
-                        </li>
-                        <li class="nav-item d-lg-none">
-                            <a class="nav-link" href="keranjang.php">Keranjang Saya</a>
-                        </li>
-                        <li class="nav-item d-lg-none">
-                            <a class="nav-link" href="notifikasi.php">Notifikasi</a>
-                        </li>
-                        <li class="nav-item d-lg-none">
-                            <a class="nav-link" href="pengaturan-akun.php">Pengaturan Akun</a>
-                        </li>
-                        <li class="nav-item d-lg-none">
-                            <a class="nav-link text-danger" href="logout.php">Logout</a>
-                        </li>
+                        <li class="nav-item d-lg-none"><hr></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link" href="dashboard.php">Pembelajaran Saya</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link" href="keranjang.php">Keranjang Saya</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link" href="notifikasi.php">Notifikasi</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link" href="pengaturan-akun.php">Pengaturan Akun</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link text-danger" href="logout.php">Logout</a></li>
+                    <?php else : ?>
+                        <li class="nav-item d-lg-none"><hr></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link" href="login.php">Log In</a></li>
+                        <li class="nav-item d-lg-none"><a class="nav-link" href="register.php">Sign Up</a></li>
                     <?php endif; ?>
                 </ul>
 
-                <div class="d-none d-lg-flex align-items-center"> <?php if (isset($_SESSION['user_id'])) : ?>
+                <div class="d-none d-lg-flex align-items-center">
+                    <?php if (isset($_SESSION['user_id'])) : ?>
                         <a href="keranjang.php" class="text-dark me-3"><i class="bi bi-cart fs-5"></i></a>
                         <a href="notifikasi.php" class="text-dark me-4"><i class="bi bi-bell fs-5"></i></a>
                         <div class="dropdown">
@@ -96,3 +93,4 @@ $current_page = basename($_SERVER['PHP_SELF']);
             </div>
         </div>
     </nav>
+    <main>
